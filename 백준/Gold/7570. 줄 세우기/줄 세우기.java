1ef14e01498f348faa365159b0d0c031f7e97ed8
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2023-04-06 00:33:18 +0900

    [Gold III] Title: 줄 세우기, Time: 1008 ms, Memory: 95084 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		int[] line = new int[N + 1];
+		int[] position = new int[N + 2];
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		for(int i = 1; i <= N; i++) {
+			line[i] = Integer.parseInt(st.nextToken());
+			position[line[i]] = i;
+		}
+		
+		int max = 0;
+		
+		for(int i = 1; i < N; i++) {
+			int now = i;
+			int idx = line[i] + 1; 
+			int next = position[idx];
+			
+			int len = 1;
+			
+			while(next > now){
+				now = position[idx];
+				next = position[++idx];
+				len++;
+				
+			}
+			if(max < len) max = len;
+		}
+		if(N == 1) {
+			max = 1;
+		}
+		System.out.println(N - max);
+	}
+}
