56a8edb78a7c1e409fddb2831e2f2eec2c689c62
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2023-05-08 01:24:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		long num = 0;
+		for(int i = 0; i < 3; i++) {
+			num += Long.parseLong(st.nextToken());
+		}
+		
+		System.out.println(num);
+	}
+}
