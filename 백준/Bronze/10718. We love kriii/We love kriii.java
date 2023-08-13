80322e9e151a8f4c094ef02336f97f299c746051
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2023-08-14 04:19:32 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i = 0; i < 2; i++) {
+			System.out.println("강한친구 대한육군");
+		}
+	}
+}
