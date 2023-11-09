e332ab1acd13ca2c31923c92d1a5d6f0e6458639
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2023-11-10 02:59:06 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String line;
+		while((line=in.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(line);
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			System.out.println(A + B);
+			
+		}
+	}
+}
