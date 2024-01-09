1c5cf8f67aa3c092e04ead9cd05d13f84e24fa69
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2024-01-09 11:38:16 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 172 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		int[][] A = new int[N][M];
+		int[][] B = new int[N][M];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for(int j = 0; j < M; j++) {
+				A[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for(int j = 0; j < M; j++) {
+				B[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				sb.append(A[i][j] + B[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
