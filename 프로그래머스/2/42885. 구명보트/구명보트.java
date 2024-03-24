9dd7b78af7e9cb3ef9bd122f886751b8c68555f2
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2024-03-24 22:43:13 +0900

    [level 2] Title: 구명보트, Time: 9.06 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length - 1;
+        
+        while(start <= end) {
+            if(people[start] + people[end] <= limit) {
+                answer++;
+                start++;
+                end--;
+            }
+            else {
+                answer++;
+                end--;
+            }
+        }
+        return answer;
+    }
+}
