76b0dace01f2c033df421d7245bb8320e1b8218b
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2024-06-29 11:12:09 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		System.out.println(n * m - 1);
+	}
+}
