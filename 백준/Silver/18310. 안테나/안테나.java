9db249ecb81f0f4d936caf33cdc6d946fc83dc57
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2024-08-29 21:16:14 +0900

    [Silver III] Title: 안테나, Time: 516 ms, Memory: 42276 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int[] nums = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		System.out.println(nums[(N - 1) / 2]);
+	}
+}
