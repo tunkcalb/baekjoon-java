b961b9b0c0eb2db98d791da63b34fd0f4162090a
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2024-09-29 22:20:43 +0900

    [Silver II] Title: K번째 소수, Time: 168 ms, Memory: 22144 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.java
+++ 백준/Silver/15965. K번째 소수/K번째 소수.java
@@ -5,13 +5,14 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = 500000;
+		int N = 8000000;
 		int K = Integer.parseInt(in.readLine());
 		
 		boolean[] nums = new boolean[N + 1];
 		int cnt = 0;
 		for(int i = 2; i <= N; i++) {
 			if(!nums[i]) cnt++;
+			else continue;
 			if(cnt == K) {
 				System.out.println(i);
 				return;
