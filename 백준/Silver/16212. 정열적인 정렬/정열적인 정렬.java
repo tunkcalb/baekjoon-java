dbc612f3e2f87297690cde608c0c57878d8591a8
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2024-10-01 21:25:22 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1200 ms, Memory: 79848 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		
+		int[] nums = new int[N];
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int num : nums) {
+			sb.append(num).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
