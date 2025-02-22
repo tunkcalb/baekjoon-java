34778692042ef3c532c185f546724dfe0a4ebf00
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-02-22 20:26:20 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 76 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1; i <= N; i++) {
+			for(int j = 0; j < N - i; j++) {
+				sb.append(" ");
+			}
+			for(int j = 0; j < i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		for(int i = N - 1; i >= 1; i--) {
+			for(int j = 0; j < N - i; j++) {
+				sb.append(" ");
+			}
+			for(int j = 0; j < i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
