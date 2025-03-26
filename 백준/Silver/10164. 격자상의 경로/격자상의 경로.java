95d8f7673751e21d838fcf200b90d91e12ec0c68
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-03-26 21:42:54 +0900

    [Silver II] Title: 격자상의 경로, Time: 64 ms, Memory: 11516 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int[][] board;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		board = new int[N + 1][M + 1];
+		int kRow = K / M + 1;
+		int kCol = K % M;
+		
+		board[1][1] = 1;
+		countPath(1, 1, kRow, kCol);
+		countPath(kRow, kCol, N, M);
+
+		System.out.println(board[N][M]);
+	}
+
+	private static void countPath(int startR, int startC, int endR, int endC) {
+
+		for (int i = startR; i <= endR; i++) {
+			for (int j = startC; j <= endC; j++) {
+				if (i > startR)
+					board[i][j] += board[i - 1][j];
+				if (j > startC)
+					board[i][j] += board[i][j - 1];
+			}
+		}
+	}
+}
