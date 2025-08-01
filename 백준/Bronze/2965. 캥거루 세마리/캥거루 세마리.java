c51beca504a01988c3f360748aa74a94b157b7f0
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-08-01 23:37:10 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+	
+		int max = Math.max((B - A), (C - B));
+		
+		System.out.println(max - 1);
+	}
+}
