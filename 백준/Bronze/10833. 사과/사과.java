161462d0dd45ff48f53d51514a5a577670d1759b
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-08-03 21:14:40 +0900

    [Bronze III] Title: 사과, Time: 72 ms, Memory: 11584 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sum += b % a;
+		}
+		
+		System.out.println(sum);
+	}
+}
