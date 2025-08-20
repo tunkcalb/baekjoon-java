fd8db5a5add86ac7fba8733c16e5a0cada0e911f
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-08-20 23:22:28 +0900

    [Bronze III] Title: 거북이, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/2959. 거북이/거북이.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = 4;
+		int[] nums = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		System.out.println(nums[0] * nums[2]);
+	}
+}
