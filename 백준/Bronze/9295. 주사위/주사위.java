3863333b68af91f58f5721585cd409ca18b601f5
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-08-25 23:35:46 +0900

    [Bronze IV] Title: 주사위, Time: 76 ms, Memory: 12160 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append("Case ").append(t).append(": ").append(a + b).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
