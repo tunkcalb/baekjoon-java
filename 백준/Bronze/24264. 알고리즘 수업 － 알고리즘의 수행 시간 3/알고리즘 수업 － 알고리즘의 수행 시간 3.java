c1d7efb5362ec2605c379e34773e8a9587037db0
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-08-26 23:26:54 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long N = Long.parseLong(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(N * N).append("\n");
+		sb.append(2);
+		
+		System.out.println(sb);
+	}
+}
