97806627dfce4340874c086bf12786f8ec686a88
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-08-29 20:07:54 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = 'z' - 'a' + 1;
+		
+		int[] cnt = new int[N];
+		
+		String line = br.readLine();
+		
+		for(int i = 0; i < line.length(); i++) {
+			cnt[line.charAt(i) - 'a']++;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < N; i++) {
+			sb.append(cnt[i]).append(" ");
+		}
+		
+		System.out.println(sb);
+	}
+}
