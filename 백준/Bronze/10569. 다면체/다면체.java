be779dc3ba645c6f162eee8a64ca5eb3c751b274
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-09-01 23:39:25 +0900

    [Bronze IV] Title: 다면체, Time: 60 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			
+			sb.append(E - V + 2).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
