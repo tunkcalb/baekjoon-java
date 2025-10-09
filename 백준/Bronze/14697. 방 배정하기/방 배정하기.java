cdcdf159f62116f8eeef3fa03460f7f4815accc2
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-10-09 17:07:28 +0900

    [Bronze II] Title: 방 배정하기, Time: 68 ms, Memory: 11540 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i <= N / A; i++) {
+			for(int j = 0; j <= N / B; j++) {
+				for(int k = 0; k <= N / C; k++) {
+					if(A * i + B * j + C * k == N) {
+						System.out.println(1);
+						return;
+					}
+				}
+			}
+		}
+		System.out.println(0);
+	}
+}
