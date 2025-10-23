c60f0f3a3380f8ecf0c2adc4bdd15848eb40e5da
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-10-23 22:52:14 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 60 ms, Memory: 11388 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			sb.append(((M * 2) - N) + " " + (M - ((M * 2) - N))).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
