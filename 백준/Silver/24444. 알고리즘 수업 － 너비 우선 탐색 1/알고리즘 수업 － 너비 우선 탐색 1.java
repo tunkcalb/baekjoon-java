2c0089fbdc0a9bfbba1a26aeffbb33b999aca55a
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-11-15 16:43:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 892 ms, Memory: 99300 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M, R;
+	static List<List<Integer>> list;
+	static int[] idxs;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		
+		list = new ArrayList<>();
+		for(int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			list.get(u).add(v);
+			list.get(v).add(u);
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			Collections.sort(list.get(i));
+		}
+		
+		idxs = new int[N + 1];
+		bfs();
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= N; i++) {
+			sb.append(idxs[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+	private static void bfs() {
+		
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(R);
+		int idx = 1;
+		while(!q.isEmpty()) {
+			
+			int now = q.poll();
+			
+			if(idxs[now] != 0) continue;
+			
+			idxs[now] = idx++;
+			
+			for(int next : list.get(now)) {
+				if(idxs[next] != 0) continue;
+				
+				q.offer(next);
+			}
+			
+		}
+	}
+}
