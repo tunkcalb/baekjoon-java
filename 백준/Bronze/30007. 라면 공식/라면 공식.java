f97c514be25df0326077363a3ee47f285f3d50f2
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2025-12-06 15:14:19 +0900

    [Bronze V] Title: 라면 공식, Time: 68 ms, Memory: 11772 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			
+			sb.append(A *(X - 1) + B).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
