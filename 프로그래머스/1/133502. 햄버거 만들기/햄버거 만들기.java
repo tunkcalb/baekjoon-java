5327d1a2596708bed50d9a8e3d953bba37bb306b
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2026-05-27 19:57:01 +0900

    [level 1] Title: 햄버거 만들기, Time: 31.36 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int ing : ingredient) {
+
+            stack.push(ing);
+
+            if (stack.size() >= 4) {
+
+                int size = stack.size();
+
+                if (stack.get(size - 4) == 1 &&
+                    stack.get(size - 3) == 2 &&
+                    stack.get(size - 2) == 3 &&
+                    stack.get(size - 1) == 1) {
+
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
